d74abb2abf61796bf9b29789c29c878aebe5f372
Nil Kantha Gaudel <nirmalgaudel63@gmail.com>
2021-11-15 11:23:48 +0545

    .

--- validators/adminValidators.js
+++ validators/adminValidators.js
@@ -4,27 +4,38 @@ const { getUserAccountByUsername, getUserAccount } = require("../models/adminMod
 function createAccountValidator() {
     return [
         body("username")
+            .trim()
             .exists()
             .withMessage("username is required")
+            .isLength({ min: 2 })
+            .withMessage("username must have at least 2 characters")
+            .isLength({ max: 20 })
+            .withMessage("username must have less than 20 characters")
             .custom(
-                username =>
+                un =>
                     new Promise(async (resolve, reject) => {
-                        const result = await getUserAccountByUsername(username).catch(e =>
-                            reject(e)
-                        );
+                        const result = await getUserAccountByUsername(un).catch(e => reject(e));
                         if (result) reject("Username Already Exists");
                         else resolve();
                     })
             ),
 
-        body("firstname").exists().withMessage("firstname is required"),
-        body("lastname").exists().withMessage("lastname is required"),
-        body("password")
+        body("firstname")
             .trim()
             .exists()
-            .withMessage("password is required")
-            .isLength({ min: 5 })
-            .withMessage("password must have at least 5 characters"),
+            .withMessage("firstname is required")
+            .isLength({ min: 2 })
+            .withMessage("firstname must have at least 2 characters")
+            .isLength({ max: 20 })
+            .withMessage("firstname must have less than 20 characters"),
+        body("lastname")
+            .trim()
+            .exists()
+            .withMessage("lastname is required")
+            .isLength({ min: 2 })
+            .withMessage("lastname must have at least 2 characters")
+            .isLength({ max: 20 })
+            .withMessage("lastname must have less than 20 characters"),
     ];
 }
 
@@ -49,8 +60,13 @@ function updateUserValidator() {
                     })
             ),
         body("username")
+            .trim()
             .exists()
             .withMessage("username is required")
+            .isLength({ min: 2 })
+            .withMessage("username must have at least 2 characters")
+            .isLength({ max: 20 })
+            .withMessage("username must have less than 20 characters")
             .custom(
                 un =>
                     new Promise(async (resolve, reject) => {
@@ -60,9 +76,23 @@ function updateUserValidator() {
                     })
             ),
 
-        body("firstname").exists().withMessage("firstname is required"),
-        body("lastname").exists().withMessage("lastname is required"),
+        body("firstname")
+            .trim()
+            .exists()
+            .withMessage("firstname is required")
+            .isLength({ min: 2 })
+            .withMessage("firstname must have at least 2 characters")
+            .isLength({ max: 20 })
+            .withMessage("firstname must have less than 20 characters"),
+        body("lastname")
+            .trim()
+            .exists()
+            .withMessage("lastname is required")
+            .isLength({ min: 2 })
+            .withMessage("lastname must have at least 2 characters")
+            .isLength({ max: 20 })
+            .withMessage("lastname must have less than 20 characters"),
     ];
 }
 
-module.exports = { loginValidator, createAccountValidator,updateUserValidator };
+module.exports = { loginValidator, createAccountValidator, updateUserValidator };
